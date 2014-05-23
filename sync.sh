2733c7dd2d7b6dc0c6c23e9f055c575505d8227c
t3rr0r <mail@t3rr0r.com>
2014-05-23 12:26:28 -0400

    disable guest access & trying to enable three finger drag

--- sync.sh
+++ sync.sh
@@ -6,4 +6,3 @@ rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
     --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~
 
 source ~/.bash_profile
-source ~/.osx
