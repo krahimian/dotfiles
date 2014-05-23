26d71669356201b4e109997647b44b057ee7e835
t3rr0r <mail@t3rr0r.com>
2014-05-23 11:00:25 -0400

    added gemfile - moved sync out of bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")"
-
-rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-    --exclude "README.md" --exclude "LICENSE-MIT.txt" -avh --no-perms . ~
-source ~/.bash_profile
-
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
 brew bundle ~/init/Caskfile
 brew bundle ~/init/Brewfile
@@ -14,12 +8,10 @@ launchctl load ~/Library/LaunchAgents/homebrew.mxcl.mysql.plist
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.elasticsearch.plist
 launchctl load ~/Library/LaunchAgents/homebrew.mxcl.redis.plist
 
-bash < <(curl -s https://rvm.beginrescueend.com/install/rvm)
+curl -L https://get.rvm.io | bash -s stable --ruby
 if [[ -s ~/.rvm/scripts/rvm ]] ; then source ~/.rvm/scripts/rvm ; fi
 rvm get head
 rvm reload
-rvm install 2.1.0
-rvm use 2.1.0 --default
 
 gem install bundler
 bundle install
@@ -36,8 +28,8 @@ npm install -g grunt-init
 npm install -g nodemon
 npm install -g cordova
 
-pip install --upgrade pip
-pip install --upgrade setuptools
+pip install --upgrade --no-use-wheel pip
+pip install --upgrade --no-use-wheel setuptools
 pip install awscli --upgrade
 pip install django
 sudo pip install carbon
